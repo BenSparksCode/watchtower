d5abc267f479dd3a27a2f82f1a3da1bd3fe7f307
Ben Sparks <bensparks0@gmail.com>
2019-09-30 22:55:08 +0200

    Added FRED API calls
    Indicators so far:
    -yield curve
    -gdp growth
    -unemployment rate

--- Server/watchtower/data-sources/fred_api.py
+++ Server/watchtower/data-sources/fred_api.py
@@ -1,3 +1,25 @@
+import requests
 from api_keys import getApiKey
 
-print(getApiKey("fred"))
+
+key = getApiKey("fred")
+url_base = "https://api.stlouisfed.org/fred/series/observations?series_id="
+url_key = "api_key="+key
+url_output = "file_type=json"
+
+indicators = {
+    "yield_curve": "T10Y2Y",
+    "gdp_growth": "A191RL1Q225SBEA",
+    "unemployment": "UNRATE",
+}
+
+def callFredApi(indicator):
+    whole_url = url_base+indicators[indicator]+"&"+url_key+"&"+url_output
+    response = requests.get(whole_url)
+    return response.json()
+
+# if(__name__ == "__main__"):
+#     final_res = callFredApi("yield_curve")
+#     print(final_res)
+
+
